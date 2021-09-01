ec8fca3e5572ec797e5bb9b5a5822f57ed1ca7fe
Terry Kim <terrykim025@gmail.com>
2021-09-01 10:59:21 +0900

    Add markdown editor text cell

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
-import CodeCell from './components/CodeCell';
+import TextEditor from './components/TextEditor';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
