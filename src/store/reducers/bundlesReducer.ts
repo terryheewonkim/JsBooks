11c9b80a04415aaf9612fbf594df592cbb39c6d4
Terry Kim <terrykim025@gmail.com>
2021-09-03 15:51:56 +0900

    move bundle logic to redux, add progress bar to code cell

--- src/store/reducers/bundlesReducer.ts
+++ src/store/reducers/bundlesReducer.ts
@@ -1,1 +1,38 @@
-export default 1;
+import produce from 'immer';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface BundlesState {
+  [key: string]:
+    | {
+        loading: boolean;
+        code: string;
+        err: string;
+      }
+    | undefined;
+}
+
+const INIT_STATE: BundlesState = {};
+
+const reducer = produce((state: BundlesState = INIT_STATE, action: Action): BundlesState => {
+  switch (action.type) {
+    case ActionType.BUNDLE_START:
+      state[action.payload.cellId] = {
+        loading: true,
+        code: '',
+        err: '',
+      };
+      return state;
+    case ActionType.BUNDLE_COMPLETE:
+      state[action.payload.cellId] = {
+        loading: false,
+        code: action.payload.bundle.code,
+        err: action.payload.bundle.err,
+      };
+      return state;
+    default:
+      return state;
+  }
+}, INIT_STATE);
+
+export default reducer;
